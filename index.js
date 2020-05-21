6bf9b8c9d7f6090762e33a05719fa48f704a310b
Evan <62257610+evanlax9@users.noreply.github.com>
2020-05-20 18:12:28 -0600

    Inquirer working

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const path = require("path");
+const fs = require("fs");
+
+
+
+
 const questions = [
+    {
+        type: "input",
+        name: "name",
+        message: "github username:",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "user email:",
+    },
+    {
+        type: "input",
+        name: "url",
+        message: "project url:",
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "title of project::",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "short description:",
+    }
 
 ];
 
@@ -6,6 +39,10 @@ function writeToFile(fileName, data) {
 }
 
 function init() {
+    inquirer.prompt(questions).then((responses) => {
+        console.log({ ...responses });
+
+    })
 
 }
 
